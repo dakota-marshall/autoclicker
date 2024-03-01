afdfbb8ccf547312e335dbba18b572bf658eeaeb
Dakota Marshall <me@dakotamarshall.net>
2024-02-29 23:11:17 -0500

    Convert click bind to be a toggle instead of hold

--- main.go
+++ main.go
@@ -21,10 +21,27 @@ func autoClicker(keystate <-chan bool, delay_chan <-chan int64) {
 
 	var state bool
 	var delay int64
+	// var toggle bool
+
+	state = false
 
 	for {
+		// Do a non blocking read on toggle state
+		// select {
+		// case toggle_read := <-togglestate:
+		// 	toggle = toggle_read
+		// default:
+		// }
 
-		state = <-keystate
+		// fmt.Println(strconv.FormatBool(toggle))
+
+		select {
+		case state_read := <-keystate:
+			fmt.Println("Value found in keystate channel:")
+			state = state_read
+			fmt.Println(strconv.FormatBool(state))
+		default:
+		}
 
 		// Dont block on delay read
 		select {
@@ -45,6 +62,9 @@ func autoClicker(keystate <-chan bool, delay_chan <-chan int64) {
 func updateKeyState(keystate chan<- bool, state bool) func(e hook.Event) {
 	return func(e hook.Event) {
 
+		fmt.Println("Keystate Event")
+		fmt.Println(strconv.FormatBool(state))
+
 		if state {
 			// Dont block to add true's to the queue
 			select {
@@ -53,19 +73,41 @@ func updateKeyState(keystate chan<- bool, state bool) func(e hook.Event) {
 			}
 		} else {
 			// Do block to add false to queue no matter what
+			fmt.Println("Block adding false to channel")
 			keystate <- state
 		}
 	}
 }
 
-func eventHooks(keystate chan<- bool, keybind string) {
+func toggleKeyState(keystate chan<- bool, togglestate chan bool) func(e hook.Event) {
+	return func(e hook.Event) {
+
+		var toggle bool
+		fmt.Println("Toggling Keystate")
+		// toggle state tracker
+		toggle = !<-togglestate
+		togglestate <- toggle
+		// Send state update to autoclicker
+		keystate <- toggle
+	}
+}
+
+func eventHooks(keystate chan<- bool, keybind string, is_toggle bool, togglestate chan bool) {
 
 	fmt.Println("Registering key events for: " + keybind)
 
-	// Register our keystate hooks
-	hook.Register(hook.KeyDown, []string{keybind}, updateKeyState(keystate, true))
+	if !is_toggle {
+		// Register our keystate hooks
+		hook.Register(hook.KeyDown, []string{keybind}, updateKeyState(keystate, true))
 
-	hook.Register(hook.KeyUp, []string{keybind}, updateKeyState(keystate, false))
+		// This seems like it is currently broken
+		// It never calls this function on a KeyUp event
+		// https://github.com/robotn/gohook/issues/31
+		// hook.Register(hook.KeyUp, []string{keybind}, updateKeyState(keystate, false))
+	} else {
+		hook.Register(hook.KeyDown, []string{keybind}, toggleKeyState(keystate, togglestate))
+	}
+
 	key_checker := hook.Start()
 	<-hook.Process(key_checker)
 
@@ -88,10 +130,10 @@ func updateKeybind(new_key string, keybind *string) {
 	}
 }
 
-func startEventHooks(keystate chan<- bool, keybind *string, status_label *widget.Label) func() {
+func startEventHooks(keystate chan<- bool, keybind *string, status_label *widget.Label, is_toggle bool, togglestate chan bool) func() {
 	return func() {
 		if status_label.Text == "Off" {
-			go eventHooks(keystate, *keybind)
+			go eventHooks(keystate, *keybind, is_toggle, togglestate)
 			status_label.SetText("On")
 		} else {
 			fmt.Println("Hooks already enabled...")
@@ -99,14 +141,22 @@ func startEventHooks(keystate chan<- bool, keybind *string, status_label *widget
 	}
 }
 
-func stopEventHooks(status_label *widget.Label) func() {
+func stopEventHooks(status_label *widget.Label, keystate chan<- bool) func() {
 	return func() {
 		if status_label.Text == "On" {
+			keystate <- false
 			hook.End()
 			status_label.SetText("Off")
 		} else {
 			fmt.Println("Hooks already disabled...")
 		}
+	}
+}
+
+func updateTogglestate(togglestate chan<- bool) func(value bool) {
+	return func(value bool) {
+		fmt.Println("Toggling state")
+		togglestate <- value
 	}
 }
 
@@ -121,12 +171,16 @@ func main() {
 	delay_chan := make(chan int64, 2)
 	delay_chan <- 0
 
+	// Toggle state tracker
+	togglestate := make(chan bool, 2)
+	togglestate <- false
+
 	var delay int64
 	delay = 0
 	keybind := "p"
 
 	// Run our key event thread
-	go eventHooks(keystate, keybind)
+	go eventHooks(keystate, keybind, true, togglestate)
 
 	// Run our autoclicker thread
 	go autoClicker(keystate, delay_chan)
@@ -145,6 +199,9 @@ func main() {
 	keybind_key := widget.NewEntry()
 	keybind_key.SetText(keybind)
 	keybind_key.OnChanged = func(input string) { updateKeybind(input, &keybind) }
+	//Toggle Box
+	// Because KeyUp is broken, setting toggle to the only option
+	// toggle_box := widget.NewCheck("Toggle instead of hold?", updateTogglestate(togglestate))
 	keybind_text := container.New(layout.NewHBoxLayout(), keybind_label, keybind_key)
 
 	// Delay box
@@ -156,8 +213,8 @@ func main() {
 	delay_text := container.New(layout.NewHBoxLayout(), delay_label, delay_key, delay_button)
 
 	// Buttons
-	hooks_start_button := widget.NewButton("Start Autoclicker", startEventHooks(keystate, &keybind, clicker_status_label))
-	hooks_stop_button := widget.NewButton("Stop Autoclicker", stopEventHooks(clicker_status_label))
+	hooks_start_button := widget.NewButton("Start Autoclicker", startEventHooks(keystate, &keybind, clicker_status_label, true, togglestate))
+	hooks_stop_button := widget.NewButton("Stop Autoclicker", stopEventHooks(clicker_status_label, keystate))
 
 	// Entire window
 	content := container.New(layout.NewVBoxLayout(), status_text, keybind_text, delay_text, hooks_start_button, hooks_stop_button)
